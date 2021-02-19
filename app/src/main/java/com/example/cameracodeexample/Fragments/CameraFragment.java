c38fb17facaea037a17cd6d502aa804b5596cf3e
aneeza812 <49375925+aneeza812@users.noreply.github.com>
2021-02-19 16:00:59 +0500

    test

---
+++ app/src/main/java/com/example/cameracodeexample/Fragments/CameraFragment.java
@@ -0,0 +1,200 @@
+package com.example.cameracodeexample.Fragments;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+
+import com.example.cameracodeexample.MainActivity;
+import com.example.cameracodeexample.R;
+import com.example.cameracodeexample.utils.DataBaseHandler;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class CameraFragment extends Fragment {
+    EditText inputImage;
+    private static final int CAMERA_REQUEST = 1888;
+    TextView text, text1;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    //Bitmap photo;
+    String photo;
+    String locationData;
+    DataBaseHandler databaseHandler;
+    private SQLiteDatabase db;
+    Bitmap theImage;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.camera_fragment, container, false);
+
+
+        // imageView =view. findViewById(R.id.imageView1);
+        text = view.findViewById(R.id.text);
+        text1 = view.findViewById(R.id.text1);
+
+        inputImage = view.findViewById(R.id.inputImageName);
+
+        databaseHandler = new DataBaseHandler(getContext());
+
+        text.setOnClickListener(
+                new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @Override
+                    public void onClick(View v) {
+                        if (getActivity().checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                                getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+
+                        ) {
+                            requestPermissions(new String[]{Manifest.permission.CAMERA,
+                                    Manifest.permission.ACCESS_FINE_LOCATION}, MY_CAMERA_PERMISSION_CODE);
+                        } else {
+                            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+                        }
+                    }
+                });
+
+        text1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) getActivity()).loadFragment(new LocalFragment(), true);
+            }
+        });
+        return view;
+    }
+
+    private void setDataToDataBase() {
+        db = databaseHandler.getWritableDatabase();
+        String name = "no name";
+        if (!inputImage.getText().toString().trim().isEmpty()) {
+            name = inputImage.getText().toString().trim();
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        String currentDateTime = sdf.format(new Date());
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(getContext())
+                .getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@androidx.annotation.NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if(location != null){
+                    Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+                    try {
+                        Address address = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1).get(0);
+                        locationData = address.getCountryName()+","+address.getAdminArea()+","+address.getLocality();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        ContentValues cv = new ContentValues();
+        cv.put(databaseHandler.KEY_IMG_URL, getEncodedString(theImage));
+        cv.put(databaseHandler.KEY_NAME, name);
+        cv.put(databaseHandler.KEY_DATE, currentDateTime);
+        cv.put(databaseHandler.KEY_LOCATION, locationData);
+
+
+        long id = db.insert(databaseHandler.TABLE_NAME, null, cv);
+        if (id < 0) {
+            Toast.makeText(getContext(), "Something went wrong. Please try again later...", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getContext(), "Add successful", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Reuqesting for premissons
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getActivity(), "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(getActivity(), "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    /**
+     * Start an activity for result
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            theImage = (Bitmap) data.getExtras().get("data");
+            photo = getEncodedString(theImage);
+            setDataToDataBase();
+        }
+    }
+
+
+    private String getEncodedString(Bitmap bitmap) {
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+
+       /* or use below if you want 32 bit images
+
+        bitmap.compress(Bitmap.CompressFormat.PNG, (0–100 compression), os);*/
+        byte[] imageArr = os.toByteArray();
+
+        return Base64.encodeToString(imageArr, Base64.URL_SAFE);
+
+    }
+
+
+}
